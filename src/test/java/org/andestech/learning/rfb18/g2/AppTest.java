719e38c2c982221d96a3b96c01251e247e429307
a0z9 <a0z9@rambler.ru>
2018-11-26 07:56:23 +0300

    1

---
+++ src/test/java/org/andestech/learning/rfb18/g2/AppTest.java
@@ -0,0 +1,31 @@
+package org.andestech.learning.rfb18.g2;
+
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class AppTest
+{
+
+    @BeforeClass
+    public void initData(){
+
+
+    }
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+    @AfterClass
+    public void tearDown()
+    {
+
+    }
+
+}
